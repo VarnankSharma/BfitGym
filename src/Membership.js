92bd5f5c53ca4a7e115202febc8b5b24b7193365
VarnankSharma <varnanksharma1@gmail.com>
2025-01-05 01:18:17 +0530

    Updated content

--- src/Membership.js
+++ src/Membership.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Membership() {
   return (
     <section id="membership">
-      <h2>Membership Plans</h2>
+      <h2 className="trainers-title">Membership Plans</h2>
       <div className="membership-container">
         <div className="membership-box">
           <h3>1 Month</h3>
--- src/PersonalTraining.js
+++ src/PersonalTraining.js
@@ -3,7 +3,7 @@ import React from 'react';
 function PersonalTraining() {
   return (
     <section id="personal-training">
-      <h2>Personal Training</h2>
+      
       <div className="personal-training-container">
         <div className="membership-box">
           <h3>Personal Training</h3>
