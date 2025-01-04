7382c225b0f706c2ca8e34bcfcb6fe5bef62db45
VarnankSharma <varnanksharma1@gmail.com>
2025-01-04 17:54:44 +0530

    Updated content

--- src/Trainers.js
+++ src/Trainers.js
@@ -4,17 +4,25 @@ function Trainers() {
   return (
     <section id="trainers">
       <h2>Meet Our Trainers</h2>
-      <div>
-        <h3>Karan Thakur</h3>
-        <p>Experience: 17 years</p>
-      </div>
-      <div>
-        <h3>Kirti</h3>
-        <p>Specializes in female fitness</p>
-      </div>
-      <div>
-        <h3>Sonu</h3>
-        <p>Zumba Trainer</p>
+      <div className="trainers-container">
+        {/* Top Trainer */}
+        <div className="top-trainer">
+          <div className="trainer-box">
+            <h3>Karan Thakur</h3>
+            <span>Experience: 17 years</span>
+          </div>
+        </div>
+        {/* Bottom Trainers */}
+        <div className="bottom-trainers">
+          <div className="trainer-box">
+            <h3>Kirti</h3>
+            <span>Specializes in female fitness</span>
+          </div>
+          <div className="trainer-box">
+            <h3>Sonu</h3>
+            <span>Zumba Trainer</span>
+          </div>
+        </div>
       </div>
     </section>
   );
