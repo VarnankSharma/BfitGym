cc40e8ab0470f2582a8c84f3e83029e0f0b43e8b
VarnankSharma <varnanksharma1@gmail.com>
2025-01-04 23:59:19 +0530

    Updated content

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 function Home() {
   return (
-    <section id="home">
+    <section id="home" className="home-section">
       <div className="welcome">
         <h2>Welcome to Be-Fit Gym</h2>
       </div>
