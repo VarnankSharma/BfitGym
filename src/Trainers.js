01ce92b84e45bea6a70c1b58fd33d7dc9cc4c600
VarnankSharma <varnanksharma1@gmail.com>
2025-01-05 01:43:14 +0530

    Updated content

--- src/Trainers.js
+++ src/Trainers.js
@@ -6,16 +6,21 @@ function Trainers() {
       <h2 className="trainers-title">Meet Our Trainers</h2>
       <div className="trainers-container">
         <div className="trainer-box top-trainer">
+          <img src="karan-image.jpg" alt="Karan Thakur" className="trainer-img" />
           <h3>Karan Thakur</h3>
           <span>Experience: 17 years</span>
         </div>
-        <div className="trainer-box">
-          <h3>Kirti</h3>
-          <span>Specializes in female fitness</span>
-        </div>
-        <div className="trainer-box">
-          <h3>Sonu</h3>
-          <span>Zumba Trainer</span>
+        <div className="trainer-row">
+          <div className="trainer-box">
+            <img src="kirti-image.jpg" alt="Kirti" className="trainer-img" />
+            <h3>Kirti</h3>
+            <span>Specializes in female fitness</span>
+          </div>
+          <div className="trainer-box">
+            <img src="sonu-image.jpg" alt="Sonu" className="trainer-img" />
+            <h3>Sonu</h3>
+            <span>Zumba Trainer</span>
+          </div>
         </div>
       </div>
     </section>
