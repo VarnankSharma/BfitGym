d5d0314be9eba5f15e56ac5ee03931300c9d3205
VarnankSharma <varnanksharma1@gmail.com>
2025-01-04 16:43:42 +0530

    Fix issue

--- src/navbar.js
+++ src/navbar.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import logo from '../assets/Logo.png';
+import logo from '../src/assets/Logo.png';
 
 function Navbar() {
   const [menuOpen, setMenuOpen] = useState(false);
