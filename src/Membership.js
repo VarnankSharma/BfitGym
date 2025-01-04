865333967e16bd07f45993e9900aac32ba2f1540
VarnankSharma <varnanksharma1@gmail.com>
2025-01-04 17:40:05 +0530

    Updated content

--- src/Membership.js
+++ src/Membership.js
@@ -1,29 +1,28 @@
+import React from 'react';
+
 function Membership() {
   return (
     <section id="membership">
       <h2>Membership Plans</h2>
-      <ul>
-        <li>
+      <div className="membership-container">
+        <div className="membership-box">
           <h3>1 Month</h3>
           <span>Rs 1300</span>
-        </li>
-        <li>
+        </div>
+        <div className="membership-box">
           <h3>3 Months</h3>
           <span>Rs 3000</span>
-        </li>
-        <li>
+        </div>
+        <div className="membership-box">
           <h3>6 Months</h3>
           <span>Rs 5500 (1 Month Free)</span>
-        </li>
-        <li>
+        </div>
+        <div className="membership-box">
           <h3>1 Year</h3>
           <span>Rs 10000 (1 Month Free + Gym Bag)</span>
-        </li>
-      </ul>
-     
-      
+        </div>
+      </div>
     </section>
-   
   );
 }
 
