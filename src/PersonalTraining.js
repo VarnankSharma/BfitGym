ef4dfa22c7d905d2671d444871cc0a5c44f20aab
VarnankSharma <varnanksharma1@gmail.com>
2025-01-04 17:50:11 +0530

    Updated content

--- src/PersonalTraining.js
+++ src/PersonalTraining.js
@@ -2,20 +2,16 @@ import React from 'react';
 
 function PersonalTraining() {
   return (
-    
     <section id="personal-training">
-      <div className="membership-box">
       <h2>Personal Training</h2>
-      <ul>
-        <li>
+      <div className="membership-container">
+        <div className="membership-box">
           <h3>Personal Training</h3>
           <span>Rs 5000/month</span>
-        </li>
-      </ul>
-      <a href="#signup" className="btn">Sign Up Now</a>
+        </div>
       </div>
+      <a href="#signup" className="btn">Sign Up Now</a>
     </section>
-    
   );
 }
 
