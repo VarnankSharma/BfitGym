6ebe4d90161734a7dcf420cf03755cd317ad24cf
VarnankSharma <varnanksharma1@gmail.com>
2025-01-04 23:30:08 +0530

    Updated content

--- src/Home.js
+++ src/Home.js
@@ -2,8 +2,8 @@ import React from 'react';
 function Home() {
   return (
     <section id="home" className="home-section">
-      <div className="w">
-        <h2 className='welcome'>Welcome to B-Fit Gym</h2>
+      <div className="welcome">
+        <h2>Welcome to B-Fit Gym</h2>
       </div>
       <p className="motto">Your journey to fitness starts here!</p>
       <p className="timing">Timings: 6:00 AM - 11:00 AM / 4:00 PM - 9:00 PM</p>
